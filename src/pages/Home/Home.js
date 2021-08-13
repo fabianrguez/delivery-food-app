ae57ded9f1eebfe03112aca796c57eb9a7c83af1
fabianrguez <fabian1041993@gmail.com>
2021-08-13 18:50:34 +0100

    working on home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,15 @@
 const Home = () => {
   return (
-    <>
-      <h1>Home</h1>
-    </>
+    <div className="dfa-home">
+      <fieldset className="dfa-home__location">
+        <i className="icon icon-marker" aria-hidden="true"></i>
+        <input type="search" name="location" placeholder="Ubicación"/>
+      </fieldset>
+      <div className="dfa-home__greeting">
+        <h3 className="dfa-home__greeting-name">Hola, Batman</h3>
+        <h1 className="dfa-home__greeting-general">Buenos días</h1>
+      </div>
+    </div>
   );
 }
 
